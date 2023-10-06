3f7b0d27622e87227262b7145e80311ccf1b0ab1
Richard-Kawenze <richardkawenze22@gmail.com>
2023-10-07 01:57:02 +0300

    tipcalculator

---
+++ tipcalculator.py
@@ -0,0 +1,7 @@
+#RICHARD MUNYWOKI KAWENZE 
+#SCT211-0053/2022
+
+totalBill=int(input("Enter the total Bill"))
+people=int(input("Enter the number of people"))
+tip=0.1*totalBill
+print("The tip to paid is ",tip)
